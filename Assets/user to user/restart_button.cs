e1ede5acfd645dbd566af66f4a9ba40ab6f7d1c3
ivanguz07 <ivan.guzman.809@my.csun.edu>
2017-11-27 16:25:31 -0800

    tictacitai
    fix bugs itai tic tac toe

--- Assets/user to user/restart_button.cs
+++ Assets/user to user/restart_button.cs
@@ -8,9 +8,19 @@ public class restart_button : MonoBehaviour {
     
     public void restart()
     {
+        tictac_cpu_mode.mode_choice = 0;
+        Scene scene = SceneManager.GetActiveScene();
+        Debug.Log(scene.name);
+        if (scene.name== "cpu vs user")
+        {
+            SceneManager.LoadScene(2);
+        }
+        else
+        {
+            SceneManager.LoadScene(1);
+        }
         
-        SceneManager.LoadScene(1);
-
+     
        
         
     }
