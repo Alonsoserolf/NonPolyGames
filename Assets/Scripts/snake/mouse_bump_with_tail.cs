5bf5d6084cb7018c48fd59aae5b6f0d59d912570
ivanguz07 <ivan.guzman.809@my.csun.edu>
2018-04-02 16:01:13 -0700

    adding snake it 4/2
    adding itai’s snake and grid

---
+++ Assets/Scripts/snake/mouse_bump_with_tail.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mouse_bump_with_tail : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+    void OnTriggerEnter(Collider other)
+    {
+
+        if (other.name == "Snake Head(Clone)")
+        {
+           
+            creat_object.did_mouse_collide_with_tail_ = true;
+            Destroy(this.gameObject);
+            Debug.Log("Destroy");
+        }
+
+        
+
+    }
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
