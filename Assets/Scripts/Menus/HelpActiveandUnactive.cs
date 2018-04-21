5c7489f93f3c6caf8ed74e19f10991817a48eef7
ivanguz07 <ivan.guzman.809@my.csun.edu>
2018-04-21 15:33:00 -0700

    fixedsoundwithhelpbutton4/21

--- Assets/Scripts/Menus/HelpActiveandUnactive.cs
+++ Assets/Scripts/Menus/HelpActiveandUnactive.cs
@@ -17,33 +17,33 @@ public class HelpActiveandUnactive : MonoBehaviour {
 		
 	}
 	public void onClickHelp(){
-		StartCoroutine (LoadingLevelClickHelp());
-		/*if (!Menuld.isLoading) {
-			Menuld.isLoading = false;
-			HelpCanvas.SetActive (true);
-		}*/
-	}
-	IEnumerator LoadingLevelClickHelp(){
-		yield return new WaitForSeconds(1f);
+		//StartCoroutine (LoadingLevelClickHelp());
 		if (!Menuld.isLoading) {
 			Menuld.isLoading = false;
 			HelpCanvas.SetActive (true);
 		}
 	}
+	/*IEnumerator LoadingLevelClickHelp(){
+		yield return new WaitForSeconds(1f);
+		if (!Menuld.isLoading) {
+			Menuld.isLoading = false;
+			HelpCanvas.SetActive (true);
+		}
+	}*/
 	public void OnClickExitHelp(){
-		StartCoroutine (LoadingLevelQuitHelp());
-		/*if (!Menuld.isLoading) {
-			Menuld.isLoading = false;
-			Menuld.buttonSelected = false;
-			HelpCanvas.SetActive (false);
-		}*/
-	}
-	IEnumerator LoadingLevelQuitHelp(){
-		yield return new WaitForSeconds(1f);
+		//StartCoroutine (LoadingLevelQuitHelp());
 		if (!Menuld.isLoading) {
 			Menuld.isLoading = false;
 			Menuld.buttonSelected = false;
 			HelpCanvas.SetActive (false);
 		}
 	}
+	/*IEnumerator LoadingLevelQuitHelp(){
+		yield return new WaitForSeconds(1f);
+		if (!Menuld.isLoading) {
+			Menuld.isLoading = false;
+			Menuld.buttonSelected = false;
+			HelpCanvas.SetActive (false);
+		}
+	}*/
 }
