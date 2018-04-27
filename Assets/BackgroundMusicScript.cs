f082b561e9774ce84a68955a08aa3a34f991aa26
Pierre Azar <pierreazar94@gmail.com>
2018-04-27 00:11:58 -0700

    Backgroud music script
    Fixed background music script.
    Can now be used to have continous music through all scenes.

--- Assets/BackgroundMusicScript.cs
+++ Assets/BackgroundMusicScript.cs
@@ -7,10 +7,12 @@ public class BackgroundMusicScript : MonoBehaviour {
 	void Awake() {
 		GameObject[] backgroundMusic = GameObject.FindGameObjectsWithTag("BackgroundMusic");
 		GameObject[] MMmusic = GameObject.FindGameObjectsWithTag("MMBackgroundMusic");
-		if (backgroundMusic.Length > 1 || MMmusic.Length >= 1) {
+		if (backgroundMusic.Length > 1) {
 			Destroy(this.gameObject);
+		} else if (MMmusic.Length >= 1) {
+			Destroy(backgroundMusic[0]);
+		} else {
+			DontDestroyOnLoad(this.gameObject);
 		}
-
-		DontDestroyOnLoad(this.gameObject);
 	}
 }
